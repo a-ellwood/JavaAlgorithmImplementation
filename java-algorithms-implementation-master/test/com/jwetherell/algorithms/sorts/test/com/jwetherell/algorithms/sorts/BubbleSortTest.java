95a27b56b7325926fe6afa0309a66574221e337d
andrea <aellwood@uoguelph.ca>
2024-11-22 17:12:51 -0500

    Lab quiz 3

---
+++ java-algorithms-implementation-master/test/com/jwetherell/algorithms/sorts/test/com/jwetherell/algorithms/sorts/BubbleSortTest.java
@@ -0,0 +1,16 @@
+package com.jwetherell.algorithms.sorts;
+
+import org.junit.jupiter.api.Test;
+
+
+import static jdk.nashorn.internal.objects.Global.print;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    public void sort() throws Exception{
+
+    }
+}
---
+++ java-algorithms-implementation-master/test/com/jwetherell/algorithms/sorts/test/com/jwetherell/algorithms/sorts/QuickSortTest.java
@@ -0,0 +1,13 @@
+package com.jwetherell.algorithms.sorts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+
+    @Test
+    public void sort() throws Exception{
+
+    }
+}
